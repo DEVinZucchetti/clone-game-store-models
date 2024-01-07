414d9cd5168de1e9fef743c78f19a80711eb2ad0
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 07:07:23 -0300

    feat: productRequirements

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
+
+use App\Http\Controllers\ProductRequirementController;
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
-
+Route::get('products_requirements', [ProductRequirementController::class, 'index']);
+Route::post('products_requirements', [ProductRequirementController::class, 'store']);
+Route::delete('products_requirements/{id}', [ProductRequirementController::class, 'destroy']);
+Route::put('products_requirements/{id}', [ProductRequirementController::class, 'update']);
