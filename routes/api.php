7fb652828a201301ebe21d26758915277f7db206
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 18:11:42 -0300

    finalizado

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\AchievementController;
+use App\Http\Controllers\AssetControler;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AvaliationController;
+use App\Http\Controllers\MarkerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductMarkerController;
+use App\Http\Controllers\RequirementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'index']);
@@ -8,3 +15,37 @@ Route::post('products', [ProductController::class, 'store']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::delete('products/{id}', [ProductController::class, 'destroy']);
 Route::put('products/{id}', [ProductController::class, 'update']);
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+Route::put('categories/{id}', [CategoryController::class, 'update']);
+
+Route::post('markers', [MarkerController::class, 'store']);
+Route::get('markers', [MarkerController::class, 'index']);
+Route::delete('markers/{id}', [MarkerController::class, 'destroy']);
+
+Route::post('products_markers', [ProductMarkerController::class, 'store']);
+Route::get('products_markers', [ProductMarkerController::class, 'index']);
+Route::delete('products_markers/{id}', [ProductMarkerController::class, 'destroy']);
+
+Route::post('avaliations', [AvaliationController::class, 'store']);
+Route::get('avaliations', [AvaliationController::class, 'index']);
+Route::put('avaliations/{id}', [AvaliationController::class, 'update']);
+Route::delete('avaliations/{id}', [AvaliationController::class, 'destroy']);
+
+Route::get('assets', [AssetControler::class, 'index']);
+Route::post('assets', [AssetControler::class, 'store']);
+Route::put('assets/{id}', [AssetControler::class, 'update']);
+Route::delete('assets/{id}', [AssetControler::class, 'destroy']);
+
+Route::get('achievements', [AchievementController::class, 'index']);
+Route::post('achievements', [AchievementController::class, 'store']);
+Route::put('achievements/{id}', [AchievementController::class, 'update']);
+Route::delete('achievements/{id}', [AchievementController::class, 'destroy']);
+
+Route::get('requirements', [RequirementController::class, 'index']);
+Route::post('requirements', [RequirementController::class, 'store']);
+Route::put('requirements/{id}', [RequirementController::class, 'update']);
+Route::delete('requirements/{id}', [RequirementController::class, 'destroy']);
