e5417f6cf5b5e7dc21c54cdeaf68693222f9e2bc
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 17:34:13 -0300

    feat: Assets

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+use App\Http\Controllers\ProductAssetControler;
 
+Route::get('ProductAssets', [ProductAssetControler::class, 'index']);
+Route::post('ProductAssets', [ProductAssetControler::class, 'store']);
+Route::put('ProductAssets/{id}', [ProductAssetControler::class, 'update']);
+Route::delete('ProductAssets/{id}', [ProductAssetControler::class, 'destroy']);
