75ea1b7ea95e655eaada21c7087b282299f193c1
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 17:24:48 -0300

    feat: Avaliation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+use App\Http\Controllers\AvaliationController;
+
+Route::post('Avaliations', [AvaliationController::class, 'store']);
+Route::get('Avaliations', [AvaliationController::class, 'index']);
+Route::put('Avaliations/{id}', [AvaliationController::class, 'update']);
+Route::delete('Avaliations/{id}', [AvaliationController::class, 'destroy']);
 
