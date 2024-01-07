188d1eaf04b4894dedb14d15e4df2441829d290a
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 17:41:58 -0300

    finalizado

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+use App\Http\Controllers\CategoryController;
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+Route::put('categories/{id}', [CategoryController::class, 'update']);
+
 
