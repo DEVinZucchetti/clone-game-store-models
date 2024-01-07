42f0ef01a1631883542a4fe1f7effa289899d8d1
Helena Delpizzo <hdelpizzo@gmail.com>
2024-01-07 16:37:58 -0300

    feat: Marker e ProductMarker

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductMarkerController;
 
+Route::post('markers', [MarkerController::class, 'store']);
+Route::get('markers', [MarkerController::class, 'index']);
+Route::delete('markers/{id}', [MarkerController::class, 'destroy']);
+
+Route::post('products_markers', [ProductMarkerController::class, 'store']);
+Route::get('products_markers', [ProductMarkerController::class, 'index']);
+Route::delete('products_markers/{id}', [ProductMarkerController::class, 'destroy']);
